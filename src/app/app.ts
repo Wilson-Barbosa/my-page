885a4c9b30360aee98579c060f3a2ff81f972cc9
Wilson-Barbosa <wilsond_barbosa@outlook.com>
2025-06-05 17:21:56 -0300

    add navbar, footer and intro sections

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
+import { Card } from "./shared/card/card";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [Navbar, Card],
+    templateUrl: './app.html',
+    styleUrl: './app.css'
 })
 export class App {
-  protected title = 'my-page';
+    protected title = 'my-page';
+
+    currentYear: number = new Date().getFullYear();
 }
