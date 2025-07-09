64730d4cf7d85f175e4404d40bcf75d0cee4b922
Wilson-Barbosa <wilsond_barbosa@outlook.com>
2025-07-08 23:17:50 -0300

    update view and add new sections
    Add social media section. Add about me page. Education section moved to about me section. Update navbar to be sticky and resizable.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    imports: [],
+    imports: [NgClass],
     templateUrl: './app.html',
     styleUrl: './app.css'
 })
 export class App {
+
     protected title = 'my-page';
 
+    isPageAtTheTop: boolean = false;
     currentYear: number = new Date().getFullYear();
-    cssClass: string = "hover:text-(--secondary-darker)";
+    cssClass: string = "hover:text-(--secondary-darker) transition-all duration-300";
+
+    /** Listens to any scroll event to determine if the nav-bar should shrink */
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+        const scrollY = window.scrollY || document.documentElement.scrollTop || 0;
+        (scrollY === 0) ? this.isPageAtTheTop = true : this.isPageAtTheTop = false;
+    }
+
+    /** Scroll the view to the top of the page */
+    sendToTop(): void {
+        document.documentElement.scrollTop = 0;
+    }
 }
